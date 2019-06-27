4d2e23bd736f436418fbdc4f07e50fec4743a015
ZivanovicLuka <lukazivanovic1995@gmail.com>
2019-06-27 16:43:46 +0200

    Init world, custom grid size, centered

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,110 @@
 
 #include "misc.hpp"
 
-void test_range();
+class World
+{
+private:
+    /* Here will be the instance stored. */
+    static World *instance;
+
+    /* Private constructor to prevent instancing. */
+    World(unsigned w, unsigned h);
+
+    unsigned screenWidth;
+    unsigned screenHeight;
+
+    unsigned innerWidth;
+    unsigned innerHeight;
+
+    unsigned xCount;
+    unsigned yCount;
+
+    unsigned cellSize;
+
+    unsigned cellMargin = 5;
+
+    std::vector<std::vector<sf::RectangleShape>> grid;
+
+public:
+    /* Static access method. */
+    static World *getInstance(unsigned w, unsigned h);
+
+    void initGrid(unsigned m, unsigned n);
+    void drawGrid(sf::RenderWindow &window);
+};
+
+/* Null, because instance will be initialized on demand. */
+World *World::instance = NULL;
+
+World *World::getInstance(unsigned w, unsigned h)
+{
+    if (instance == NULL)
+    {
+        instance = new World(w, h);
+    }
+
+    return instance;
+}
+
+World::World(unsigned w, unsigned h)
+// : screenWidth(w), screenHeight(h)
+{
+    std::cout << w << " " << h << std::endl;
+    screenWidth = w;
+    screenHeight = h;
+}
+
+void World::initGrid(unsigned m, unsigned n)
+{
+    xCount = m;
+    yCount = n;
+
+    unsigned cellSizeX = (screenWidth - cellMargin) / xCount;
+    unsigned cellSizeY = (screenHeight - cellMargin) / yCount;
+
+    cellSize = std::min(cellSizeX, cellSizeY) - cellMargin;
+
+    innerWidth = ((cellSize + cellMargin) * xCount) - cellMargin;
+    innerHeight = ((cellSize + cellMargin) * yCount) - cellMargin;
+
+    unsigned topPadding = (screenHeight % innerHeight) / 2;
+    unsigned leftPadding = (screenWidth % innerWidth) / 2;
+
+    grid.resize(xCount);
+    for (unsigned i = 0; i < xCount; ++i)
+        grid[i].resize(yCount);
+
+    for (unsigned i = 0; i < xCount; ++i)
+    {
+        for (unsigned j = 0; j < yCount; ++j)
+        {
+            grid[i][j] = sf::RectangleShape(sf::Vector2f(cellSize, cellSize));
+            grid[i][j].setPosition(leftPadding + i * (cellMargin + cellSize),
+                                   topPadding + j * (cellMargin + cellSize));
+            grid[i][j].setFillColor(sf::Color::Blue);
+        }
+    }
+};
+
+void World::drawGrid(sf::RenderWindow &window)
+{
+    for (unsigned i = 0; i < xCount; ++i)
+    {
+        for (unsigned j = 0; j < yCount; ++j)
+        {
+            window.draw(grid[i][j]);
+        }
+    }
+}
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    const unsigned w = 800;
+    const unsigned h = 600;
+    sf::RenderWindow window(sf::VideoMode(w, h), "SFML works!");
+
+    World *world = World::getInstance(w, h);
+    world->initGrid(20, 10);
 
     while (window.isOpen())
     {
@@ -21,16 +118,14 @@ int main()
         }
 
         window.clear();
-        window.draw(shape);
+
+        world->drawGrid(window);
+
         window.display();
     }
+
     std::cout << misc::meaning_of_life() << std::endl;
     misc::test_range();
 
     return 0;
 }
-
-
-
-
-
