17e7470cc05ee4062e00783e4a068779228c332a
libido <leonardovlibido@gmail.com>
2019-07-02 00:01:27 +0200

    add controls for animation speed and grid size

--- src/game.cpp
+++ src/game.cpp
@@ -6,12 +6,19 @@ const unsigned Game::s_windowWidth = sf::VideoMode::getDesktopMode().width * 2 /
 const unsigned Game::s_windowHeight = sf::VideoMode::getDesktopMode().height * 2 / 3;
 
 sf::Time Game::s_tick = sf::seconds(1.0f);
+sf::Time Game::s_fastest_tick = sf::seconds(0.1f);
+sf::Time Game::s_slowest_tick = sf::seconds(2.0f);
+sf::Time Game::s_tick_change = sf::seconds(0.1f);
+int Game::s_xGridSize = 20;
+int Game::s_yGridSize = 10;
+int Game::s_xGridChange = 10;
+int Game::s_yGridChange = 5;
 
 Game::Game()
     : m_window(sf::VideoMode(s_windowWidth, s_windowHeight), "Game of Life")
 {
     m_world = World::getInstance(s_windowWidth, s_windowHeight);
-    m_world->initGrid(20, 10);
+    m_world->initGrid(s_xGridSize, s_yGridSize);
 }
 
 Game::~Game()
@@ -39,8 +46,7 @@ void Game::Run()
 
 void Game::Update(float dt)
 {
-    std::cout << "One second has passed" << std::endl;
-    std::cout << dt << std::endl;
+    (void)dt;
 
     if (m_animate)
         m_world->updateGrid();
@@ -53,26 +59,72 @@ void Game::Render()
     m_window.display();
 }
 
+void Game::processKeyboardEvents(sf::Event& event)
+{
+    switch (event.key.code)
+    {
+    case sf::Keyboard::Escape:
+    case sf::Keyboard::X:
+        // close the application
+        m_window.close();
+        break;
+    case sf::Keyboard::G:
+        // start/stop the animation
+        m_animate = !m_animate;
+        break;
+
+    case sf::Keyboard::R:
+        // clear the grid
+        m_world->clearGrid();
+        break;
+
+    case sf::Keyboard::H:
+        // higher speed
+        if (s_tick > s_fastest_tick)
+            s_tick -= s_tick_change;
+        break;
+
+    case sf::Keyboard::L:
+        // lower speed
+        if (s_tick < s_slowest_tick)
+            s_tick += s_tick_change;
+        break;
+
+    case sf::Keyboard::B:
+        // bigger grid
+        s_xGridSize += s_xGridChange;
+        s_yGridSize += s_yGridChange;
+
+        m_world->initGrid(s_xGridSize, s_yGridSize);
+        break;
+
+    case sf::Keyboard::S:
+        // smaller grid
+        if (s_xGridSize > s_xGridChange)
+            s_xGridSize -= s_xGridChange;
+        if (s_yGridSize > s_yGridChange)
+            s_yGridSize -= s_yGridChange;
+
+        m_world->initGrid(s_xGridSize, s_yGridSize);
+        break;
+    default:
+        break;
+    }
+}
+
 void Game::ProcessEvents()
 {
     sf::Event event;
 
     while (m_window.pollEvent(event))
     {
-        if ((event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) || (event.type == sf::Event::Closed))
+        if (event.type == sf::Event::Closed)
         {
             m_window.close();
         }
 
-        if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::G)
-        {
-            m_animate = !m_animate;
-        }
-
-        if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::R)
-        {
-            m_world->clearGrid();
-        }
+        if (event.type == sf::Event::KeyPressed)
+            processKeyboardEvents(event);
 
         if (event.type == sf::Event::MouseMoved)
             m_world->handleHover(event);
--- src/game.hpp
+++ src/game.hpp
@@ -15,6 +15,16 @@ private:
     sf::RenderWindow m_window;
     World *m_world;
     static sf::Time s_tick;
+    static sf::Time s_fastest_tick;
+    static sf::Time s_slowest_tick;
+    static sf::Time s_tick_change;
+
+    static int s_xGridSize;
+    static int s_yGridSize;
+    static int s_xGridChange;
+    static int s_yGridChange;
+
+
     bool m_animate = false;
 
     const static unsigned s_windowWidth;
@@ -23,6 +33,7 @@ private:
     void Update(float dt);
     void Render();
     void ProcessEvents();
+    void processKeyboardEvents(sf::Event& event);
 };
 
 #endif /* GAME_H */
